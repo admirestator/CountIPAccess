8feea0bf1e4d888144c0370c070c6703ce24996e
admirestator <admirestator@gmail.com>
2012-03-12 15:45:44 +0800

    finished sort the map by value

--- loganalysis.cpp
+++ loganalysis.cpp
@@ -51,26 +51,35 @@ void LogAnalysis::getin_ip() {
             ++ret.first->second;
     }
 
-    /*
-      for (cnt_cstidx = cnt.begin (); cnt_cstidx != cnt.end(); ++cnt_cstidx) {
-      cout << cnt_cstidx->first.c_str() << "\t" << cnt_cstidx->second << endl;
-      }
-    */
-
+    //sort by value
+    for(cnt_cstidx = cnt.begin(); cnt_cstidx != cnt.end(); ++cnt_cstidx) {
+        cnt_reverse.insert( make_pair(cnt_cstidx->second, cnt_cstidx->first) );
+    }
 }
 
+
+void LogAnalysis::getout_rst() {
+    for (cnt_reverseidx = cnt_reverse.rbegin(); cnt_reverseidx != cnt_reverse.rend(); ++cnt_reverseidx) {
+        fip->GetAddressByIp(cnt_reverseidx->second.c_str(), addr.country, addr.location);
+        outfile << left << setw(15) << cnt_reverseidx->second << "\t\t"
+                << left << cnt_reverseidx->first << "\t\t"
+                << left << setw(28) << addr.country << "\t"
+                << addr.location << endl;
+	}
+}
+
+/*
 void LogAnalysis::getout_rst() {
     for (cnt_cstidx = cnt.begin(); cnt_cstidx != cnt.end(); ++cnt_cstidx) {
         fip->GetAddressByIp(cnt_cstidx->first.c_str(), addr.country, addr.location);
         outfile << left << setw(15) << cnt_cstidx->first << "\t\t"
                 << left <<  cnt_cstidx->second << "\t\t"
-                << left << setw(36) << addr.country << "\t"
+                << left << setw(28) << addr.country << "\t"
                 << addr.location << endl;
 	}
 }
-
-/////////interface above
-
+*/
+/////////public interface above
 ifstream& LogAnalysis::open_file (ifstream & in, const string & file)
 {
     in.close ();
